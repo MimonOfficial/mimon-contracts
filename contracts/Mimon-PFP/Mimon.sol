5bcd51a4ce1446d4d8c3d51fd209c354717a41fe
elixirevo <elixirevocation@gmail.com>
2021-12-28 15:54:42 +0900

    init

---
+++ contracts/Mimon-PFP/Mimon.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+import "../openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "../openzeppelin/contracts/utils/Context.sol";
+import "../openzeppelin/contracts/utils/Counters.sol";
+import "../openzeppelin/contracts/access/Ownable.sol";
+
+contract Mimon is Context, ERC721, ERC721Enumerable, AccessControlEnumerable, Ownable {
+	using Counters for Counters.Counter;
+
+	string TOKEN_NAME = "Mimon";
+	string TOKEN_SYMBOL = "MIMON";
+	uint256 MAX_CLONES_SUPPLY = 10000;
+	string private _baseTokenURI;
+	address public minterContract;
+	address public devAddress;
+	address public lotusContract;
+	address public openseaContract;
+
+	Counters.Counter private _tokenIdTracker;
+
+	modifier onlyMinter() {
+		require(_msgSender() == minterContract);
+		_;
+	}
+
+	modifier onlyDev() {
+		require(_msgSender() == devAddress);
+		_;
+	}
+
+	constructor(string memory baseTokenURI) ERC721(TOKEN_NAME, TOKEN_SYMBOL) {
+		_baseTokenURI = baseTokenURI;
+	}
+
+	function mint(address to) external virtual onlyMinter {
+		require(totalSupply() < 10000, "Mint end.");
+		_mint(to, _tokenIdTracker.current());
+		_tokenIdTracker.increment();
+	}
+
+	function massTransferFrom(
+		address from,
+		address to,
+		uint256[] memory _myTokensId
+	) public {
+		require(_myTokensId.length <= 100, "Can only transfer 100 Clones at a time");
+		for (uint256 i = 0; i < _myTokensId.length; i++) {
+			transferFrom(from, to, _myTokensId[i]);
+		}
+	}
+
+	function setMinterContract(address saleContract) public onlyDev {
+		minterContract = saleContract;
+	}
+
+	function setBaseURI(string memory baseURI) public onlyDev {
+		_baseTokenURI = baseURI;
+	}
+
+	function getBaseURI() public view returns (string memory) {
+		return _baseURI();
+	}
+
+	function setDevAddress(address _devAddress) public onlyOwner {
+		devAddress = _devAddress;
+	}
+
+	function setLotusContract(address _lotus) public onlyDev {
+		lotusContract = address(_lotus);
+	}
+
+	function setOpenseaContract(address _opensea) public onlyDev {
+		openseaContract = address(_opensea);
+	}
+
+	/**
+	 * Override isApprovedForAll to auto-approve OS's proxy contract
+	 */
+	function isApprovedForAll(address _owner, address _operator) public view override returns (bool isOperator) {
+		// if OpenSea's ERC721 Proxy Address is detected, auto-return true
+		if (_operator == openseaContract) {
+			return true;
+		}
+		return ERC721.isApprovedForAll(_owner, _operator);
+	}
+
+	function supportsInterface(bytes4 interfaceId) public view virtual override(AccessControlEnumerable, ERC721, ERC721Enumerable) returns (bool) {
+		return super.supportsInterface(interfaceId);
+	}
+
+	function _baseURI() internal view virtual override returns (string memory) {
+		return _baseTokenURI;
+	}
+
+	function _beforeTokenTransfer(
+		address from,
+		address to,
+		uint256 tokenId
+	) internal virtual override(ERC721, ERC721Enumerable) {
+		super._beforeTokenTransfer(from, to, tokenId);
+	}
+}
